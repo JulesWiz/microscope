7149278de475a3ad316b63881ea3c13ae9aa5c3e
JulesWiz <ngjulie0203@gmail.com>
2014-05-02 10:31:35 +0800

    Added basic posts list template and static data

---
+++ client/view/posts/posts_list.js
@@ -0,0 +1,20 @@
+var postsData = [
+  {
+    title: 'Introducing Julie',
+    author: 'JulesWiz',
+    url: 'http://ngjulie.meteor.com'
+  },
+  {
+    title: 'Verybite',
+    author: 'Jules Verybite',
+    url: 'http://verybite.com'
+  },
+  {
+    title: 'Julie Ng',
+    author: 'Linked in',
+    url: 'http://linkedin.com/julieasia'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
