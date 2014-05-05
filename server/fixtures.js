f9435726dfe60f36335c91b49bfd7d50a812d7de
JulesWiz <ngjulie0203@gmail.com>
2014-05-05 16:32:15 +0800

    Added comments collection, pub/sub, and fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,19 +1,55 @@
 if (Posts.find().count() === 0) {
-  Posts.insert({
+  var now = new Date().getTime();
+
+  var julId = Meteor.users.insert({
+    profile: { name: 'Jules' }
+  });
+
+  var jul = Meteor.users.findOne(julId);
+
+  var harId = Meteor.users.insert({
+    profile: { name: 'Harry' }
+  });
+
+  var har = Meteor.users.findOne(harId);
+
+  var introduceId = Posts.insert({
     title: 'Introducing Julie',
-    author: 'JulesWiz',
-    url: 'http://ngjulie.meteor.com'
+    userId: jul._id,
+    author: jul.profile.name,
+    url: 'http://ngjulie.meteor.com',
+    submitted: now - 7 * 3600 * 1000
+  });
+
+  Comments.insert({
+    postId: introduceId,
+    userId: jul._id,
+    author: jul.profile.name,
+    submitted: now - 5 * 3600 * 1000,
+    body: 'How do you create the developer account?'
+  });
+
+  Comments.insert({
+    postId: introduceId,
+    userId: har._id,
+    author: har.profile.name,
+    submitted: now - 3 * 3600 * 1000,
+    body: 'Just log in to www.meteor.com.'
   });
 
   Posts.insert({
     title: 'Verybite',
-    author: 'Jules Verybite',
-    url: 'http://verybite.com'
+    userId: har._id,
+    author: har.profile.name,
+    url: 'http://verybite.com',
+    submitted: now - 10 * 3600 * 1000
   });
 
   Posts.insert({
     title: 'Julie Ng',
-    author: 'Linked in',
-    url: 'http://linkedin.com/julieasia'
+    userId: jul._id,
+    author: jul.profile.name,
+    url: 'http://linkedin.com/julieasia',
+    submitted: now - 12 * 3600 * 1000
   });
 }
