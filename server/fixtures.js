a7fcd6560d30348a2c05e612b32ff4168281ef05
JulesWiz <ngjulie0203@gmail.com>
2014-05-06 11:45:07 +0800

    Added enough posts that pagination is necessary

--- server/fixtures.js
+++ server/fixtures.js
@@ -55,4 +55,15 @@ if (Posts.find().count() === 0) {
     submitted: now - 12 * 3600 * 1000,
     commentsCount: 0
   });
+
+  for (var i = 0; i < 10; i++ ) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: jul.profile.name,
+      userId: jul._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: now - i * 3600 * 1000,
+      commentsCount: 0
+    });
+  }
 }
